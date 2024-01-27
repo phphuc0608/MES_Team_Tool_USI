12d19343aae5c76b7d2b74c9088c82e49dd38433
phphuc0608 <phuc31082002@gmail.com>
2024-01-27 16:01:17 +0700

    config router

--- src/router/index.js
+++ src/router/index.js
@@ -1,20 +1,18 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import WipQuery from '../views/WipQuery.vue'
+import UnionAll from '../views/UnionAll.vue'
 
 const routes = [
   {
     path: '/',
-    name: 'home',
+    name: 'wip-query',
     component: WipQuery
   },
   {
-    path: '/about',
-    name: 'about',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/AboutView.vue')
-  }
+    path: '/union-all',
+    name: 'union-all',
+    component: UnionAll
+  },
 ]
 
 const router = createRouter({
