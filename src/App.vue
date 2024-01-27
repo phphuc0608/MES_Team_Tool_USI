62a4530b433a3b363755a29f6632a2e5dca6bb8b
phphuc0608 <phuc31082002@gmail.com>
2024-01-27 15:00:25 +0700

    first commit

---
+++ src/App.vue
@@ -0,0 +1,43 @@
+<template>
+  <nav class="p-3 d-flex align-items-center navbar navbar-expand-lg">
+    <img :src="require('@/assets/logo.png')" class="navbar-brand" alt="Logo">
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+      <div class="navbar-nav">
+        <router-link class="nav-link" to="/">Wip Query</router-link> 
+        <router-link class="nav-link" to="/about">Union all</router-link>
+      </div>
+    </div>
+  </nav>
+  <router-view/>
+</template>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box; 
+}
+nav{
+  box-shadow: 0 4px 8px 0 rgba(77, 76, 76, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19);
+  background-image: url('./assets/bg.jfif');
+  background-position: center;
+  background-repeat: no-repeat;
+}
+ nav .nav-link{
+    text-decoration: none;
+    font-size: 20px!important;
+    color: #2b52c9!important;   
+ }
+ nav .nav-link:hover{
+    color: #122459!important;
+    background-color: #DDDDDD;
+    border-radius: 10px;
+ }
+
+</style>
---
+++ src/views/WipQuery.vue
@@ -0,0 +1,169 @@
+<template>
+  <form method="">
+    <div id="select_container" class="d-flex justify-content-center align-items-center">
+      <div class="col-md-6 mt-3 p-2">
+        <label for="work_order">
+          <div class="label_select">Select work order</div>
+        </label>
+        <select name="work_order" id="work_order" multiple>
+          <option v-for="work_order in work_orders" :value="work_order.value">{{ work_order.name }}</option>      
+        </select>
+      </div>
+      <div class="col-md-6 mt-3 p-2">
+        <label for="group_name">
+          <div class="label_select">Select group name</div>
+        </label>
+        <select name="group_name" id="group_name" class="form-select p-2">
+          <option disabled selected value> -- select an option -- </option>
+          <option v-for="group_name in group_names" :value="group_name.value">{{ group_name.name }}</option>
+        </select>
+      </div>
+    </div>
+    <div class="p-1">
+      <button class="btn btn-success m-1" @:click="clearButton">Query <i class="bi bi-database-fill-check"></i></button>
+      <button class="btn btn-danger">Clear <i class="bi bi-x-circle-fill"></i></button>
+    </div>
+  </form>
+  <div class="p-2 label_select">RESULT:</div>
+  <div class="col-md-12 table_container mt-3">
+    <table class="table table-striped table-bordered table-hover table-primary table-responsive custom-width">
+      <thead>
+        <tr>
+          <th>NO</th>
+          <th>PANEL_SN</th>
+          <th>SERIAL_NUMBER</th>
+          <th>MO_NUMBER</th>
+          <th>CONFIG</th>
+          <th>SN_SEQ</th>
+          <th>LOCATION</th>
+          <th>REEL_ID</th>
+          <th>COMP_PART_NO</th>
+          <th>LOT_NO</th>
+          <th>DATE_CODE</th>
+          <th>VENDOR</th>
+          <th>APN</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr>
+          <td>1</td>
+          <td>HA5026110207Y6</td>
+          <td>HMH22730HAJ17PK5Z</td>
+          <td>1000112-VZR301</td>
+          <td>VZR3</td>
+          <td>1</td>
+          <td>R2110</td>
+          <td>TC-0220623-2989</td>
+          <td>107S00328-01</td>
+          <td>LM2111308</td>
+          <td>20220422</td>
+          <td>THIN FILM</td>
+          <td></td>
+        </tr>
+      </tbody>
+    </table>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'WipQuery',
+  data() {
+    return {
+      work_orders: [
+        { name: '1002991-VYJ105', value: '1002991-VYJ105' },
+        { name: 'Two', value: 'B' },
+        { name: 'Three', value: 'C' },
+        { name: 'Four', value: 'D' },
+        { name: 'Five', value: 'E' },
+        { name: 'Six', value: 'F' },
+        { name: 'Seven', value: 'G' },
+        { name: 'Eight', value: 'H' },
+        { name: 'Nine', value: 'I' },
+        { name: 'Ten', value: 'J' },
+        { name: 'Eleven', value: 'K' },
+        { name: 'Twelve', value: 'L' },
+        { name: 'Thirteen', value: 'M' },
+        { name: 'Fourteen', value: 'N' },
+        { name: 'Fifteen', value: 'O' },
+        { name: 'Sixteen', value: 'P' },
+        { name: 'Seventeen', value: 'Q' },
+        { name: 'Eighteen', value: 'R' },
+        { name: 'Nineteen', value: 'S' },
+        { name: 'Twenty', value: 'T' },
+        { name: 'Twenty-one', value: 'U' },
+        { name: 'Twenty-two', value: 'V' },
+        { name: 'Twenty-three', value: 'W' },
+        { name: 'Twenty-four', value: 'X' },
+        { name: 'Twenty-five', value: 'Y' },
+        { name: 'Twenty-six', value: 'Z' },
+      ],
+      group_names: [
+        { name: 'AOI1-B', value: 'AOI1-B  ' },
+        { name: 'Two', value: 'B' },
+        { name: 'Three', value: 'C' },
+        { name: 'Four', value: 'D' },
+        { name: 'Five', value: 'E' },
+        { name: 'Six', value: 'F' },
+        { name: 'Seven', value: 'G' },
+        { name: 'Eight', value: 'H' },
+        { name: 'Nine', value: 'I' },
+        { name: 'Ten', value: 'J' },
+        { name: 'Eleven', value: 'K' },
+        { name: 'Twelve', value: 'L' },
+        { name: 'Thirteen', value: 'M' },
+        { name: 'Fourteen', value: 'N' },
+        { name: 'Fifteen', value: 'O' },
+        { name: 'Sixteen', value: 'P' },
+        { name: 'Seventeen', value: 'Q' },
+        { name: 'Eighteen', value: 'R' },
+        { name: 'Nineteen', value: 'S' },
+        { name: 'Twenty', value: 'T' },
+        { name: 'Twenty-one', value: 'U' },
+        { name: 'Twenty-two', value: 'V' },
+        { name: 'Twenty-three', value: 'W' },
+        { name: 'Twenty-four', value: 'X' },
+        { name: 'Twenty-five', value: 'Y' },
+        { name: 'Twenty-six', value: 'Z' },
+      ],
+    };
+  },
+  mounted() {
+    new MultiSelectTag('work_order')
+  },
+  methods: {
+    clearButton(event) {
+      event.preventDefault();
+      document.getElementById("work_order").value = "";
+      document.getElementById("group_name").value = "";
+    },
+  },
+}
+</script>
+
+<style>
+*{
+  padding: 0;
+  margin: 0;
+  box-sizing: border-box;
+}
+.drawer, .rounded, .drawer .input-body .input, .form-select{
+  border: 1px solid rgb(150, 148, 148)!important;
+}
+.input-container{
+  border-right: 1px solid rgb(150, 148, 148)!important;
+}
+.label_select{
+  font-size: 18px;
+  font-weight: bold;
+  color: #2b52c9;
+}
+.table_container{
+  width: 100%;
+  overflow-x: auto;
+}
+table{
+  width: 100%;
+  border-collapse: collapse;
+}
+</style>
